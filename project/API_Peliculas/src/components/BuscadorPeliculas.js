b5d1da68e8ffea41ae459e7a58fa57db73072ddd
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-04-05 08:50:56 -0600

    Separando los estilos en una carpeta

--- project/API_Peliculas/src/components/BuscadorPeliculas.js
+++ project/API_Peliculas/src/components/BuscadorPeliculas.js
@@ -6,8 +6,8 @@ import {
   Text,
   FlatList,
   Image,
-  StyleSheet,
 } from "react-native";
+import { styles } from "../styles/BuscadorPeliculasStyles";
 
 export const BuscadorPeliculas = () => {
   const urlBase = "https://api.themoviedb.org/3/search/movie";
@@ -66,56 +66,3 @@ export const BuscadorPeliculas = () => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    padding: 20,
-  },
-  title: {
-    fontSize: 24,
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  searchSection: {
-    flexDirection: "row",
-    justifyContent: "center",
-    marginBottom: 20,
-  },
-  input: {
-    width: 300,
-    padding: 10,
-    borderColor: "#ccc",
-    borderWidth: 1,
-    borderRadius: 4,
-    marginRight: 10,
-  },
-  movieList: {
-    marginTop: 20,
-  },
-  movieCard: {
-    backgroundColor: "#fff",
-    padding: 20,
-    borderRadius: 4,
-    marginBottom: 20,
-  },
-  movieImage: {
-    width: "100%",
-    height: 300,
-    marginBottom: 10,
-  },
-  movieTitle: {
-    fontSize: 18,
-    marginBottom: 5,
-  },
-  boton: {
-    backgroundColor: "#007bff",
-    padding: 10,
-    borderRadius: 5,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  textoBoton: {
-    color: "#ffffff",
-    fontSize: 16,
-  },
-});
--- project/API_Peliculas/src/screens/LoginScreen.js
+++ project/API_Peliculas/src/screens/LoginScreen.js
@@ -1,12 +1,6 @@
 import React, { useState } from "react";
-import {
-  View,
-  Text,
-  TextInput,
-  StyleSheet,
-  Image,
-  Pressable,
-} from "react-native";
+import { View, Text, TextInput, Image, Pressable } from "react-native";
+import { styles } from "../styles/LoginScreenStyles";
 import { useAuth } from "../context/AuthContext";
 export const LoginScreen = ({ navigation }) => {
   const { login } = useAuth();
@@ -94,83 +88,3 @@ export const LoginScreen = ({ navigation }) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#252222",
-  },
-  title: {
-    fontSize: 32,
-    fontWeight: "bold",
-    marginBottom: 20,
-    color: "#FFFFFF",
-    textAlign: "center",
-  },
-  textPressable: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-    textAlign: "center",
-  },
-  input: {
-    width: "80%",
-    height: 50,
-    borderWidth: 1,
-    borderColor: "#000000",
-    borderRadius: 5,
-    marginBottom: 20,
-    paddingLeft: 10,
-    backgroundColor: "#636363",
-    elevation: 2,
-  },
-  button: {
-    width: "80%",
-    height: 50,
-    backgroundColor: "#6C63FF",
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 5,
-    marginTop: 20,
-    elevation: 3,
-  },
-  inputContainer: {
-    width: "80%",
-    flexDirection: "row",
-    alignItems: "center",
-    marginBottom: 20,
-  },
-  iconContainer: {
-    paddingRight: 10,
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 18,
-    fontWeight: "bold",
-  },
-  logo: {
-    width: 200,
-    height: 100,
-    marginTop: 50,
-  },
-  buttonContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-    marginTop: 20,
-  },
-  loginText: {
-    color: "#FFFFFF",
-    marginBottom: 10,
-  },
-  logoContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-  },
-  logoApp: {
-    width: 40,
-    height: 40,
-    marginHorizontal: 10,
-  },
-});
