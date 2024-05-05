c0260c028ecf8a7bc335b73cb0fa4d0881234441
Enrique-Zepeda <enriquezepeda60z@gmail.com>
2024-05-04 19:43:57 -0600

    Mejorando drawer

--- project/API_Peliculas/src/routes/DrawerMenu.js
+++ project/API_Peliculas/src/routes/DrawerMenu.js
@@ -1,16 +1,96 @@
 import React from "react";
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import { StyleSheet } from "react-native";
+import {
+  createDrawerNavigator,
+  DrawerContentScrollView,
+  DrawerItemList,
+  DrawerItem,
+} from "@react-navigation/drawer";
+import { Box, Icon, NativeBaseProvider } from "native-base";
+import LottieView from "lottie-react-native";
 import { BuscadorPeliculas } from "../components/BuscadorPeliculas";
 import { ProfileScreen } from "../screens/ProfileScreen";
+import { Ionicons } from "@expo/vector-icons";
+
+const CustomDrawerContent = (props) => {
+  return (
+    <NativeBaseProvider>
+      <DrawerContentScrollView {...props} style={styles.drawerBackground}>
+        <Box style={styles.drawerHeader}>
+          <LottieView
+            source={{
+              uri: "https://lottie.host/f2229f7b-d624-416e-a146-b820b40d4908/yvDgucEKn5.json",
+            }}
+            autoPlay
+            loop
+            style={{
+              width: "100%",
+              height: "100%",
+            }}
+          />
+        </Box>
+        <DrawerItemList {...props} />
+      </DrawerContentScrollView>
+    </NativeBaseProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  drawerBackground: {
+    backgroundColor: "#1f2937",
+  },
+  drawerHeader: {
+    height: 130,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 const Drawer = createDrawerNavigator();
 
 const DrawerMenu = () => {
   return (
-    <Drawer.Navigator initialRouteName="BuscadorPeliculas">
-      <Drawer.Screen name="BuscadorPeliculas" component={BuscadorPeliculas} />
-      <Drawer.Screen name="Perfil" component={ProfileScreen} />
-    </Drawer.Navigator>
+    <NativeBaseProvider>
+      <Drawer.Navigator
+        initialRouteName="BuscadorPeliculas"
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+      >
+        <Drawer.Screen
+          name="BuscadorPeliculas"
+          component={BuscadorPeliculas}
+          options={{
+            drawerIcon: () => (
+              <Icon
+                as={Ionicons}
+                name="film-outline"
+                color="#9993FF"
+                size={30}
+              />
+            ),
+            drawerLabelStyle: {
+              color: "#FFFFFF",
+            },
+          }}
+        />
+        <Drawer.Screen
+          name="Perfil"
+          component={ProfileScreen}
+          options={{
+            drawerIcon: () => (
+              <Icon
+                as={Ionicons}
+                name="person-outline"
+                color="#9993FF"
+                size={30}
+              />
+            ),
+            drawerLabelStyle: {
+              color: "#FFFFFF",
+            },
+          }}
+        />
+      </Drawer.Navigator>
+    </NativeBaseProvider>
   );
 };
 
